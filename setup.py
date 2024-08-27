bdfb808ac5f8c3b1125ce3fef11d769c8acf2375
w. Patrick Gale <41551429+kuhlaid@users.noreply.github.com>
2024-08-27 19:27:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from DvApiMod-pip-package import __version__
+
+setup(
+    name='DvApiMod-pip-package',
+    version=__version__,
+    url='https://github.com/kuhlaid/DvApiMod5.13',
+    author='w. Patrick Gale',
+    author_email='w.patrick.gale@gmail.com',
+    py_modules=['DvApiMod-pip-package'],
+)
