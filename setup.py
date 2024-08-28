b20f0c07253247e784dd140f301694e3f4e34781
w. Patrick Gale <41551429+kuhlaid@users.noreply.github.com>
2024-08-28 07:38:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,2 @@
 # setup.py shim for use with applications that require it.
 __import__("setuptools").setup()
-
-# from setuptools import setup
-
-# from DvApiMod_pip_package import __version__
-
-# setup(
-#     name='DvApiMod_pip_package',
-#     version=__version__,
-#     url='https://github.com/kuhlaid/DvApiMod5.13',
-#     author='w. Patrick Gale',
-#     author_email='w.patrick.gale@gmail.com'
-# )
