163cb63bcc0c5a42134715eadeb6ac0df22ffb37
w. Patrick Gale <w.patrick.gale@unc.edu>
2024-08-30 10:04:12 -0400

    testing versions

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import versioneer
 # setup.py shim for use with applications that require it.
 __import__("setuptools").setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=1.0.1,
 )
