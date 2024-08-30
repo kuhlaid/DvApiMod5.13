e5f2f8a788fc9a1153f3eff5ef34190113baba04
w. Patrick Gale <w.patrick.gale@unc.edu>
2024-08-30 11:34:19 -0400

    version testing

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 # NOTE: we need to change the version when we push an update so that users will not need to restart their Jupyter Server to install the latest package and so users know the version they are installing
 # TODO: we could try https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package for automating the version number generation
 
+# pull in the __version__ constant from the package
+from DvApiMod-pip-package import __version__
+
 __import__("setuptools").setup(
     version=__version__
 )
