f3983111a772a5c81a79f545d69925d405c4bb0e
w. Patrick Gale <w.patrick.gale@unc.edu>
2024-08-30 12:40:04 -0400

    version testing

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # setup.py shim for use with applications that require it.
 # NOTE: we need to change the version when we push an update so that users will not need to restart their Jupyter Server to install the latest package and so users know the version they are installing
-# TODO: we could try https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package for automating the version number generation
+# If a 'version' file can be generated automatically from GitHub actions then we could use that
 
 __import__("setuptools").setup(
     version="1.0.2"
